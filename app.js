efdce2cca8db20053bbab5b06beaa84675949c88
Felo Vilches <felovilches@gmail.com>
2017-07-04 08:47:13 -0400

    gruntless

--- app.js
+++ app.js
@@ -26,27 +26,20 @@
 // > Note: This is not required in order to lift, but it is a convenient default.
 process.chdir(__dirname);
 
+var args = process.argv.slice(2);
+function hasArg(arg){
+  return args.indexOf(arg) != -1;
+}
 
-
-// Attempt to import `sails` dependency, as well as `rc` (for loading `.sailsrc` files).
 var sails;
 var rc;
-try {
-  sails = require('sails');
-  rc = require('sails/accessible/rc');
-} catch (e) {
-  console.error('Encountered an error when attempting to require(\'sails\'):');
-  console.error(e.stack);
-  console.error('--');
-  console.error('To run an app using `node app.js`, you usually need to have a version of `sails` installed in the same directory as your app.');
-  console.error('To do that, run `npm install sails`');
-  console.error();
-  console.error('Alternatively, if you have sails installed globally (i.e. you did `npm install -g sails`), you can use `sails lift`.');
-  console.error('When you run `sails lift`, your app will still use a local `./node_modules/sails` dependency if it exists,');
-  console.error('but if it doesn\'t, the app will run with the global sails instead!');
-  return;
-}//-•
+Sails = require('sails').constructor;
+rc = require('sails/accessible/rc');
+sails = new Sails();
 
-
-// Start server
-sails.lift(rc('sails'));
+if(hasArg('--no-grunt')){
+  sails.log('Running with --no-grunt switch');
+  sails.lift({hooks: { grunt: false }});
+} else {
+  sails.lift(rc('sails'));
+}
