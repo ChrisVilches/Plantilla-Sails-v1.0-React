01501fffe3bae9acc667a70334ec4354f64a44f2
Felo Vilches <felovilches@gmail.com>
2017-07-04 02:51:09 -0400

    grunt

--- tasks/register/compileAssets.js
+++ tasks/register/compileAssets.js
@@ -14,11 +14,9 @@
 module.exports = function(grunt) {
   grunt.registerTask('compileAssets', [
     'clean:dev',
-    // 'jst:dev',
     'less:dev',
     'sass:dev',
     'copy:dev',
-    // 'coffee:dev',
     'webpack:dev'
   ]);
 };
